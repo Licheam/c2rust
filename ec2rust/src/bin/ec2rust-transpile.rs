e8e8803346c2aac4ad4f58ab35f450c1a9a88312
Leachim <32847549+Licheam@users.noreply.github.com>
2024-08-19 05:50:28 +0000

    change name to avoid conflic

--- ec2rust/src/bin/ec2rust-transpile.rs
+++ ec2rust/src/bin/ec2rust-transpile.rs
@@ -1,10 +1,10 @@
-use c2rust::process_args;
-use c2rust::Args;
+use ec2rust::process_args;
+use ec2rust::Args;
 use clap::Parser;
 
 fn main() {
     let args = Args::parse();
     let (tcfg, cc_json_path, extra_args) = process_args(args);
     let extra_args = extra_args.iter().map(AsRef::as_ref).collect::<Vec<_>>();
-    c2rust_transpile::export(tcfg, &cc_json_path, &extra_args);
+    c2rust_transpile::transpile(tcfg, &cc_json_path, &extra_args);
 }
